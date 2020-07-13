fc9cd5db1c45458d1a68c2414885e6dba88d21db
season <mimi871006@gmail.com>
2020-07-13 21:36:51 +0800

    record

--- record.py
+++ record.py
@@ -1,9 +1,10 @@
 import dataset
-oldDB = dataset.connect('mysql+pymysql://root:tNMW9ksfylH1oosQ@localhost/bravelog')
-newDB = dataset.connect('mysql+pymysql://root:tNMW9ksfylH1oosQ@localhost/bravelog_new')
+# tNMW9ksfylH1oosQ
+oldDB = dataset.connect('mysql+pymysql://root:season1006@localhost/bravelog')
+newDB = dataset.connect('mysql+pymysql://root:season1006@localhost/bravelog_new')
 old_race = oldDB['race']
-old_race - oldDB['athlete']
-old_result = oldDB['di_result']
+old_athlete = oldDB['athlete']
+old_result = oldDB['sportsnet_result']
 new_record = newDB['record']
 
 oldDB.begin()
@@ -14,7 +15,7 @@ try:
     statement = 'SELECT * \
                 FROM `event` e, \
                       (SELECT * \
-                       FROM `athlete` a, `di_result` r  \
+                       FROM `athlete` a, `sportsnet_result` r  \
                        WHERE a.`AthleteDataId`=r.`DataId`) ar \
                 WHERE e.EventId=ar.AthleteEventId'
 
@@ -23,56 +24,25 @@ try:
     for row in oldDB.query(statement):
         id += 1
         
-        cp_config = [
-            {
-                'CP_Mode': 'Gun',
-                'CP_Time': row['TimeGun']
-            },
-            {
-                'CP_Mode': 'Start',
-                'CP_Time': row['TimeStart']
+        # for cp_config
+        cp_config = [{
+                        'CP_Mode': 'Gun',
+                        'CP_Time': row['TimeGun']
+                    },{
+                        'CP_Mode': 'Start',
+                        'CP_Time': row['TimeStart']
+                    },{
+                        'CP_Mode': 'End',
+                        'CP_Time': row['finishTime']
+                    }]
+        for i in range(1,13):
+            col = f'TimeCheck0{i}' if i<10 else f'TimeCheck{i}'
+            temp = {
+                'CP_Mode': f'CP{i}',
+                'CP_Time': row[col]
             }
-            {
-                'CP_Mode': 'CP1',
-                'CP_Time': row['TimeCheck01']
-            },
-            {
-                'CP_Mode': 'CP2',
-                'CP_Time': row['TimeCheck02']
-            },
-            {
-                'CP_Mode': 'CP3',
-                'CP_Time': row['TimeCheck03']
-            },
-            {
-                'CP_Mode': 'CP4',
-                'CP_Time': row['TimeCheck04']
-            },
-            {
-                'CP_Mode': 'CP5',
-                'CP_Time': row['TimeCheck05']
-            },
-            {
-                'CP_Mode': 'CP6',
-                'CP_Time': row['TimeCheck06']
-            },
-            {
-                'CP_Mode': 'CP7',
-                'CP_Time': row['TimeCheck07']
-            },
-            {
-                'CP_Mode': 'CP8',
-                'CP_Time': row['TimeCheck08']
-            },
-            {
-                'CP_Mode': 'CP9',
-                'CP_Time': row['TimeCheck09']
-            },
-            {
-                'CP_Mode': 'End',
-                'CP_Time': row['finishTime']
-            }
-        ]
+            cp_config.append(temp)
+        
         query = {
             'id': id,
             'uid': id,
@@ -94,8 +64,7 @@ try:
             'is_disqualified': 0,
             'is_passed': 0
         }
-    
-    new_race.insert(query)
+        new_record.insert(query)
     newDB.commit()
     print("success")
 except Exception as err:
