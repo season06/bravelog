89dea94f0ad87c15cf3454a5909597f27ac0e5fc
season <mimi871006@gmail.com>
2020-07-27 15:19:45 +0800

    [fix] myredis add comment

--- myredis.py
+++ myredis.py
@@ -10,29 +10,11 @@ class redisDB():
         r = redis.Redis(**REDIS_CONFIG)
         return r
 
-    # def getAllKey(self):
-    #     all_key = self.r.keys()
-    #     print(all_key)
-
-    # def setKey(self, key, value):
-    #     self.r.set('food', 'beef')
-    
-    # def getKey(self, key):
-    #     value = self.r.get(key)
-    #     print(value)
-
-    # def getList(self): # O(N)
-
-    # def getHash(self): # O(1)
-
-    # def getSet(self):
-
-    # def getSortSet(self):
-
 def main():
     r = redisDB().connect()
-    p = r.pipeline()
+    p = r.pipeline()    # create a pipeline
 
+    # insert user info
     for i in range(10):
         user = 'user' + chr(i+65)
         account = str(i*10)
@@ -44,10 +26,10 @@ def main():
         p.hset(user, 'phone', phone)
 
         p.incr('user_number')
-
     
     p.execute()
 
+    # search user
     username = 'userA'
     if r.exists(username) == None:
         print(username + ' is not exist')
@@ -55,7 +37,6 @@ def main():
         print(r.hgetall(username)) # O(N)
 
     print(r.keys())
-    
     print('user number: ' + r['user_number'])
 
 if __name__ == "__main__":
