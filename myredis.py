91abc42147713fe550433c4a14a9a7cd3266323f
season06 <mimi871006@gmail.com>
2020-08-04 17:45:59 +0800

    [add] redis insert user time random

--- myredis.py
+++ myredis.py
@@ -2,6 +2,8 @@ import redis
 import time
 import schedule
 from datetime import datetime, timedelta
+import dataset
+import random
 
 class redisDB():
     def __init__(self):
@@ -49,23 +51,59 @@ class redisDB():
             key_arr = key.split('_')
 
             now_time_str = time.strftime('%Y%m%d')
-            now_time = datetime.today().strptime(now_time_str, '%Y%m%d')
+            now_time = datetime.strptime(now_time_str, '%Y%m%d')
             contest_time = datetime.strptime(key_arr[0], "%Y%m%d")
 
             minus_day = now_time - contest_time
             if minus_day > timedelta(days=2):
                 self.r.delete(key)
+    
+    def insert_user_random_time(self):
+        pipe = self.r.pipeline()
+
+        hash_table_name = '2020080932_user_result'
+        with dataset.connect('mysql+pymysql://user:password@172.105.206.159/test') as db:
+            for row in db['marathon']:
+                hash_key = f"3_{row['number']}"
+                hash_value = {
+                    'race_id': 3,
+                    'number': row['number'],
+                    'group': row['group'],
+                    'name': row['name'],
+                    'gender': row['gender'],
+                    'person_finish_time': get_random_time()
+                }
+                pipe.hset(hash_table_name, hash_key, str(hash_value))
+            pipe.execute()
+
+def get_random_time():
+    # time range
+    a1=(2020,7,7,1,45,0,0,0,0)
+    a2=(2020,7,7,2,15,59,0,0,0)
+    start=time.mktime(a1)
+    end=time.mktime(a2)
+
+    # random time
+    t = random.randint(start,end)
+    date_touple = time.localtime(t)
+    date_str = time.strftime("%H:%M:%S",date_touple)
+    return date_str
 
 def main():
     Redis = redisDB()
     r = Redis.connect()
 
-    Redis.insert_user_info('20200729')
-    Redis.insert_user_info('20200629')
-    print(r.keys())
+    # Redis.insert_user_info('20200803')
+    # Redis.insert_user_info('20200629')
+    # schedule.every().wednesday.at('11:00').do(Redis.check_time_expire)
 
-    schedule.every().wednesday.at('17:49').do(Redis.check_time_expire)
-    print(r.keys())    
+    Redis.insert_user_random_time()
+    
+    hash_table_name = '2020080932_user_result'
+    key_list = list(r.hgetall(hash_table_name))
+    value_list = list(r.hgetall(hash_table_name).values())
+    for i in range(len(key_list)):
+        print(key_list[i], ':: ', value_list[i])
 
 if __name__ == "__main__":
     main()
